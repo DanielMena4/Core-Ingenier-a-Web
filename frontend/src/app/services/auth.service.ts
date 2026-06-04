6459281295f001e4e7541df4ac974b9fe56d0c5b
Danielmena4 <daniel__mena@hotmail.com>
2026-06-04 13:17:51 -0500

    Typo en environments

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators';
-import { environment } from '../../environmets/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
